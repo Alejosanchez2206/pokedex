6d92ee5dbf65dc0ff6de96f9ccae279f68ef0811
Alejo Sanchez <alejoh908@gmail.com>
2022-04-01 21:42:14 -0500

    Proyecto terminado

--- src/componentes/pokedexScreen.js
+++ src/componentes/pokedexScreen.js
@@ -1,24 +1,49 @@
 import React from 'react';
-
+import ImgCarga from './Pikachu.gif';
 export default function PokedexScreen({ pokemon }) {
     let UrlImage = '';
-    if(pokemon){
+    let NamePokemons = '';
+    let TypePokemons = []
+    if (pokemon) {
         UrlImage = pokemon.sprites.other["official-artwork"].front_default;
+        NamePokemons = pokemon.name.toUpperCase();
+        TypePokemons = pokemon
     }
-    console.log(UrlImage)
-   // let UrlImage = pokemon.sprites.other["official-artwork"].front_default ? pokemon.sprites.other["official-artwork"].front_default : '';
+
     return (
         <>
             <div className="pokedex-screen-containe">
                 <div className="pokedex-screen">
-                    <div className="pokemon-info">
-                        <h2 className="pokemon-name">{pokemon.name}</h2>
-                        <img className="pokemon-img"
-                           src={UrlImage}
+                    {pokemon ? <>
+                        <div className="pokemon-info">
+                            <h2 className="pokemon-name">{NamePokemons}</h2>
+                            <img className="pokemon-img"
+                                src={UrlImage}
+                                alt={NamePokemons}
+                            />
+                            <ul className="pokemon-stat">
+
+                                {pokemon.stats.map(item => (
+                                    <>
+                                        <li align="left">
+                                            <span className="stat-name">{item.stat.name.toUpperCase()}: </span>
+                                            <span className="item-value">{item.base_stat}</span>
+                                        </li>
+
+                                    </>
+                                ))}
+
+
+                            </ul>
+                        </div>
+                    </> : <>
+                        <img
+                            src={ImgCarga}
+                            alt="Aun no hay ningun pokemon"
+                            className="pokedex-no-screen"
                         />
-                        <ul className="pokemon-stat">
-                        </ul>
-                    </div>
+                    </>}
+
                 </div>
             </div>
         </>
